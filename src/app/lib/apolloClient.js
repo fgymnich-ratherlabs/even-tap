ecf6d8e93b44b8ddddf717c5653b1eeb73bcb0b7
fgymnich-ratherlabs <franciscogymnich@ratherlabs.com>
2024-08-23 19:25:45 -0300

    Revert "Feature/events"

--- src/app/lib/apolloClient.js
+++ src/app/lib/apolloClient.js
@@ -1,26 +1,9 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-// Configura el enlace HTTP a tu servidor GraphQL
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql', // Cambia esta URL según sea necesario
-});
-
-// Configura el enlace de autenticación (opcional)
-const authLink = setContext((_, { headers }) => {
-  // Obtén el token de autenticación desde localStorage o cookies
-  const token = localStorage.getItem('auth-token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-// Crea la instancia de Apollo Client
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql', // URL de tu backend GraphQL
+  }),
   cache: new InMemoryCache(),
 });
 
